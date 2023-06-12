a8953ba30c0083a1ca7bcb09b577a99efb7486ca
James Watson <jameywatson134@gmail.com>
2023-06-12 00:51:25 -0700

    final commit w/ ninja bonuses

--- server.py
+++ server.py
@@ -5,13 +5,33 @@ app.secret_key = 'TOPSECRET' # set a secret key for security purposes
 
 @app.route('/')          
 def initialCount():
+    if 'integer' in session:
+        session['integer'] +=1
+        print('count it!')
+        print(session['integer'])
+    else:
+        print("not counting")
+        session['integer'] = 1
+
     return render_template('index.html')
 
+@app.route('/plus')
+def plusNumber():
+
+    session['integer'] +=1
+    print('count it 2x!')
+    print(session['integer'])
+    return redirect('/')
+
+
+@app.route('/destroy_session')
+def Destroy():
+    session.pop('integer')		# clears a specific key
+    session.clear()		# clears all keys
+    return redirect('/')
+
+
+
+
 if __name__=="__main__":
-    app.run(debug=True, port=5005) 
-
-
-
-
-
-
+    app.run(debug=True, port=5005)
